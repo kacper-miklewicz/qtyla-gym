39a4266796a06b10a7c7a340a3fcef17c3d3d258
kacper-miklewicz <kacper.miklewicz@gmail.com>
2026-02-27 10:23:07 +0100

    fix phone number

--- app/components/constants.ts
+++ app/components/constants.ts
@@ -1,5 +1,5 @@
 export const QTYLA_EMAIL = "qtyla.club@gmail.com";
 
-export const QTYLA_PHONE = "+48533877837";
+export const QTYLA_PHONE = "+48533877387";
 
-export const QTYLA_PHONE_DISPLAY = "533 877 837";
+export const QTYLA_PHONE_DISPLAY = "533 877 387";
